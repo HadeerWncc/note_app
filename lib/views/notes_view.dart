f02b03589c682c65ed14c59296a64e2c09c8f62b
HadeerWncc <hadeerwncc@gmail.com>
2024-08-13 11:50:34 +0200

    add border radis to button sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -11,6 +11,8 @@ class NotesView extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16)),
               context: context,
               builder: (context) {
                 return const AddNewNote();
@@ -27,5 +29,3 @@ class NotesView extends StatelessWidget {
     );
   }
 }
-
-
